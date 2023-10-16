65085538de2f77ad70bd02a68bcc4094edcf6762
Diego Florença <diegoflorenca@Diego-Air.local>
2023-10-16 07:58:51 -0700

    registre challenges app to project urls

--- monthly_challenges/monthly_challenges/urls.py
+++ monthly_challenges/monthly_challenges/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # adds all the urls from challenges/urls.py to the main urls.py
+    path('challenges/', include('challenges.urls'))
 ]
