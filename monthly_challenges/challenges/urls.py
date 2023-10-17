96afd31301eed26c7cc2d4c715e98a76fa47e205
Diego Florença <diego.florenca@gmail.com>
2023-10-17 07:33:07 -0700

    class 24 path converters

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<month>", views.monthly_challenge)
+    # handle URLs containing a number
+    path("<int:month>", views.monthly_challenge_by_number),
+    # handle URLs containing a string
+    path("<str:month>", views.monthly_challenge)
 ]
