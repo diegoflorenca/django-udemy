6cf0dd7be1ac47c38144c4e26bb5e89481e6dca0
Diego Florença <diego.florenca@gmail.com>
2023-10-17 08:15:37 -0700

    class 27  reverse function

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # handle URLs containing a number
     path("<int:month>", views.monthly_challenge_by_number),
     # handle URLs containing a string
-    path("<str:month>", views.monthly_challenge)
+    path("<str:month>", views.monthly_challenge, name="month-challenge")
 ]
