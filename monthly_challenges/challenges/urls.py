cd7c0179fd92b3770e7912e6e8568aa6ca837c6c
Diego Florença <diego.florenca@gmail.com>
2023-10-18 07:22:46 -0700

    class 29 - Add basic HTML to the response

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # handle /challenges/ calls
+    path("", views.index),
     # handle URLs containing a number
     path("<int:month>", views.monthly_challenge_by_number),
     # handle URLs containing a string
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -18,6 +18,20 @@ monthly_challenges = {
 }
 
 
+def index(request):
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{
+            capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
 
@@ -35,4 +49,4 @@ def monthly_challenge(request, month):
         challenge_text = monthly_challenges[month]
     except:
         return HttpResponseNotFound("This month is not supported!")
-    return HttpResponse(challenge_text)
+    return HttpResponse(f"<h1>{challenge_text}</h1>")
