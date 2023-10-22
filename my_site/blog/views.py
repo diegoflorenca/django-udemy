03fae16ef2644f1076ed87c4b0339a88a6f3a184
Diego Florença <diego.florenca@gmail.com>
2023-10-22 12:29:17 -0700

    class 62 - add single post page

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -8,5 +8,7 @@ def starting_page(request):
 def posts(request):
     return render(request, "blog/all-posts.html")
 
-def post_detail(request, post_slug):
-    pass #return render(request, "blog/post.html")
+def post_detail(request, slug):
+    return render(request, "blog/post-details.html", {
+        "slug": slug
+    })
