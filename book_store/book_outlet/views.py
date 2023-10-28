f7b9801f41c17ec4f75952f4739ca23a6085385e
Diego Florença <diego.florenca@gmail.com>
2023-10-28 10:07:19 -0700

    class 86 - render book details

--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, "book_outlet/index.html")
+    books = Book.objects.all()
+    return render(request, "book_outlet/index.html", {
+        "books": books
+    })
+
+
+def book_detail(request, id):
+    # try:
+    #     book = Book.objects.get(id=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book, id=id) # a shortcut for the above code.
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling
+    })
