06ac9b28bd61fd003eecb3112211041c5f2ddda8
Diego Florença <diego.florenca@gmail.com>
2023-11-07 07:48:27 -0800

    feat: create blog models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=200)
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=30)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=200)
+    image = models.CharField(max_length=100)
+    date = models.DateField()
+    slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    content = models.TextField()
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="posts")
+    tags = models.ManyToManyField(Tag)
