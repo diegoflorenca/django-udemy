07b2f6322b317670ed6cf5ac600a069e184bdab3
Diego Florença <diego.florenca@gmail.com>
2023-11-14 08:19:25 -0800

    class 140 - form validation and customization

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,15 +1,23 @@
 from django.shortcuts import render
+from .forms import ReviewForm
 
 # Create your views here.
 
 
 def review(request):
     if request.method == 'POST':
-        entered_username = request.POST['username']
-        print(entered_username)
-        return render(request, "reviews/thank_you.html")
+        form = ReviewForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+            return render(request, "reviews/thank_you.html")
     else:
-        return render(request, "reviews/index.html")
-    
+        form = ReviewForm()
+
+    return render(request, "reviews/index.html", {
+        "form": form
+    })
+
+
 def thank_you(request):
     return render(request, "reviews/thank_you.html")
