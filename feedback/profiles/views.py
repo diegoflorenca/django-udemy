f5a07388a1e04b5fd430a091d64730a55da024b9
Diego Florença <diego.florenca@gmail.com>
2024-02-25 12:10:07 -0800

    class 164 - Setup the upload file form and view

--- feedback/profiles/views.py
+++ feedback/profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 
@@ -9,4 +10,6 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        image = request.FILES['image']
+        print(image)
+        return HttpResponseRedirect("/profiles")
