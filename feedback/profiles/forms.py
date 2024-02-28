5e9f97943fc99c4ae3f22c55cf1f9963efd4d998
Diego Florença <diego.florenca@gmail.com>
2024-02-28 07:55:26 -0800

    class 169 - Using CreateView to simplify the view code

--- feedback/profiles/forms.py
+++ feedback/profiles/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class ProfileForm(forms.Form):
+    # This form has not been used because we are extending CreateView on the CreateProfileView
     user_image = forms.FileField()
 
     labels = {
