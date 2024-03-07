ea5f0bf23edcdf3808069ba9275240343baae2a3
Diego Florença <diego.florenca@gmail.com>
2024-03-07 08:34:02 -0800

    class 188 - Convert views into classes

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.starting_page, name="starting-page"),
-    path("posts/", views.posts, name="posts-page"),
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("posts/", views.AllPostsViews.as_view(), name="posts-page"),
     path("posts/comment/", views.AddCommentView.as_view(), name="add-comment"),
     path("posts/read-later/", views.ReadLaterView.as_view(), name="read-later"),
     # <slug:slug> is a path converter provided by Django
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
 ]
